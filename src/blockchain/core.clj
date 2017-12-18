d93f8449990589150b6b61973fa519ef12359135
Ben Brinckerhoff <ben@bbrinck.com>
2017-12-17 20:28:47 -0700

    Adds next-idx

--- src/blockchain/core.clj
+++ src/blockchain/core.clj
@@ -1,9 +1,6 @@
 (ns blockchain.core
   (:require [clojure.spec.alpha :as s]
-            [expound.alpha :as expound]
-            )
-  )
-
+            [expound.alpha :as expound]))
 
 (s/def :bc/sha (s/and string?
                       #(re-matches #"[A-Fa-f0-9]+" %)))
@@ -17,23 +14,26 @@
 (s/def :bc/transaction (s/keys :req
                                [:bc/sender
                                 :bc/recipient
-                                :bc/amount]
-                               ))
-(s/def :bc/transactions (s/coll-of :bc/transactions
+                                :bc/amount]))
+(s/def :bc/transactions (s/coll-of :bc/transaction
                                    :kind vector?))
+
+(s/def :bc/index nat-int?)
+(s/def :bc/timestamp pos?)
+(s/def :bc/proof pos-int?)
+(s/def :bc/prev-hash :bc/sha)
 
 (s/def :bc/block (s/keys :req
                          [:bc/index
                           :bc/timestamp
                           :bc/transactions
                           :bc/proof
-                          :bc/prev-hash]
-                         ))
+                          :bc/prev-hash]))
 
 (s/def :bc/chain (s/coll-of :bc/block :kind vector?))
 
 (s/def :bc/bc (s/keys :req [:bc/transactions
-                             :bc/chain]))
+                            :bc/chain]))
 
 (s/fdef add-tx
         :args (s/cat :bc :bc/bc
@@ -42,15 +42,28 @@
                      :amount :bc/amount)
         :ret :bc/bc)
 (defn add-tx [bc sender recipient amount]
-  
-  )
+  (update bc
+          :bc/transactions
+          conj
+          {:bc/sender sender
+           :bc/recipient recipient
+           :bc/amount amount}))
+
+(s/fdef next-idx
+        :args (s/cat :bc :bc/bc)
+        :ret :bc/index)
+(defn next-idx [bc]
+  (-> bc
+      :bc/chain
+      last
+      :bc/indix
+      (or 0)))
 
 (comment
-  (require '[orchestra.spec.test :as st])
-  (set! s/*explain-out* expound/printer)
-  (st/instrument)
-  
-  (add-tx nil nil nil nil)
-  )
+  (add-tx {:bc/chain []
+           :bc/transactions []} "abc" "def" 5))
 
+(comment (require '[orchestra.spec.test :as st])
+         (set! s/*explain-out* expound/printer)
+         (st/instrument))
 
--- test/blockchain/core_test.clj
+++ test/blockchain/core_test.clj
@@ -1,7 +1,27 @@
 (ns blockchain.core-test
   (:require [clojure.test :refer :all]
-            [blockchain.core :refer :all]))
+            [blockchain.core :refer :all]
+            [clojure.spec.alpha :as s]
+            [expound.alpha :as expound]
+            [orchestra.spec.test :as st]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn instrument [f]
+  (set! s/*explain-out* expound/printer)
+  (st/instrument)
+  (f))
+
+(use-fixtures :once instrument)
+
+(deftest test-add-tx
+  (is (= #:bc {:chain [],
+               :transactions
+               [#:bc {:sender "abc", :recipient "def", :amount 5}]}
+         (add-tx {:bc/chain []
+                  :bc/transactions []} "abc" "def" 5))))
+
+(deftest test-next-idx
+  (is (= 0
+         (next-idx
+          #:bc {:chain [],
+                :transactions
+                [#:bc {:sender "abc", :recipient "def", :amount 5}]}))))
